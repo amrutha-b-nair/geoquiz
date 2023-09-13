a03a19dd78ea8b526f3d23e9b69ceb206cd80920
amrutha-b-nair <amruthabn00@gmail.com>
2023-09-13 14:50:16 +0200

    With all country names and images

--- quiz.py
+++ quiz.py
@@ -1,81 +1,119 @@
-import tkinter as tk
-from tkinter import messagebox
+import csv
+from tkinter import *
 from PIL import Image, ImageTk
 
+# Read the CSV file
+def read_csv(filename):
+    questions = []
+    answers = []
+    with open(filename, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            questions.append(row['Image_path'])
+            answers.append(row['Country'])
+    return questions, answers
 
-# Define a list of quiz questions and answers, along with corresponding image file paths.
-questions = [
-    {
-        "question": "Name the country",
-        "answer": "France",
-        "image_path": "/home/allu/quiz/sample_images/France.png",
-    },
-    {
-        "question": "Name the country",
-        "answer": "Spain",
-        "image_path": "/home/allu/quiz/sample_images/Spain.png",
-    },
-
-]
-
-# Define a function to check the user's answer.
-def check_answer():
-    user_answer = entry.get()
-    correct_answer = questions[current_question]["answer"]
-
-    if user_answer.lower() == correct_answer.lower():
-        messagebox.showinfo("Correct", "Your answer is correct!")
-    else:
-        messagebox.showerror("Incorrect", f"Sorry, the correct answer is {correct_answer}.")
-
-    next_question()
-
-# Define a function to display the next question.
-def next_question():
-    global current_question
-    current_question += 1
-
-    if current_question < len(questions):
-        display_question()
-    else:
-        messagebox.showinfo("Quiz Complete", "Congratulations! You have completed the quiz.")
-        window.quit()
-
-# Define a function to display the current question and image.
-def display_question():
-    question_label.config(text=questions[current_question]["question"])
-    entry.delete(0, tk.END)
+# Initialize the quiz
+def start_quiz():
+    global current_question, score
+    current_question = 0
+    score = 0
+    result_label.place_forget()
+    start_button.place_forget()
+    score_label.pack(pady=10)
+    image_label.pack(pady=10)
+    answer_label.pack(side = LEFT, padx = 5)
+    answer_entry.pack(side = RIGHT)
+    submit_button.pack(pady=10)
+    finish_button.pack()
+    update_score_label()
+    display_question()
     
-    # Load and display the image.
-    image_path = questions[current_question]["image_path"]
-    img = Image.open(image_path)
-    img = img.resize((300, 200), Image.LANCZOS)
-    photo = ImageTk.PhotoImage(img)
-    image_label.config(image=photo)
-    image_label.image = photo
 
-# Create the main window.
-window = tk.Tk()
-window.title("Quiz")
-window.geometry("400x300")
+# Display the current question
+def display_question():
+    global current_question, score
+    if current_question < len(questions):
+        image_path = questions[current_question]
+        img = Image.open(image_path)
+        width, height = img.size
+        to_resize = float(400/max(width,height))
+        img = img.resize((int(width*to_resize), int(height*to_resize)), Image.LANCZOS)
+        photo = ImageTk.PhotoImage(img)
+        image_label.config(image=photo)
+        image_label.image = photo
 
-current_question = 0
+        answer_entry.delete(0, END)
+    else:
+        result_label.config(text=f"Your Score: {score}/{len(questions)}")
 
-# Create widgets for the question, image, and answer input.
-question_label = tk.Label(window, text="", font=("Arial", 14))
-question_label.pack(pady=10)
 
-image_label = tk.Label(window)
-image_label.pack()
 
-entry = tk.Entry(window, font=("Arial", 12))
-entry.pack(pady=10)
+# Check the user's answer
+def check_answer():
+    global current_question, score
+    user_answer = answer_entry.get()
+    if user_answer.lower() == answers[current_question].lower():
+        score += 1
+    current_question += 1
+    display_question()
+    update_score_label()
 
-check_button = tk.Button(window, text="Check Answer", command=check_answer)
-check_button.pack()
 
-# Start the quiz with the first question.
-display_question()
+def update_score_label():
+    score_label.config(text=f"Score: {score}/{len(questions)}")
 
-# Start the Tkinter main loop.
-window.mainloop()
+def finish_quiz():
+    result_label.place(relx=0.5, rely=0.4, anchor=CENTER)
+    start_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+    result_label.config(text=f"Your Score: {score}/{len(questions)}")
+    image_label.pack_forget()
+    answer_label.pack_forget()
+    submit_button.pack_forget()
+    answer_entry.pack_forget()
+    finish_button.pack_forget()
+    score_label.pack_forget()
+    
+
+# Load questions and answers from CSV
+questions, answers = read_csv('selected_names.csv')
+
+# Create the quiz GUI
+root = Tk()
+root.title("Quiz")
+root.geometry("600x700")
+
+frame1 = Frame(root)
+frame1.pack(pady=10)  # Add vertical space
+
+frame2 = Frame(root)
+frame2.pack(pady=10) 
+
+frame3 = Frame(root)
+frame3.pack(pady=10) 
+
+image_label = Label(frame1)
+
+
+answer_label = Label(frame2, text="Your Answer:")
+
+
+answer_entry = Entry(frame2)
+
+
+submit_button = Button(frame3, text="Submit", command=check_answer)
+
+
+start_button = Button(root, text="Start Quiz", command=start_quiz)
+start_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+# start_button.pack()
+
+finish_button = Button(frame3, text="Finish Quiz", command=finish_quiz)
+
+result_label = Label(root, text="")
+# result_label.pack()
+
+score_label = Label(frame1, text="")
+
+
+root.mainloop()
